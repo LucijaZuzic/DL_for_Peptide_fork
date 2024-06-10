db1549d4914bc48ba1496813d1fb30bcea3456e2
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-06-10 14:38:35 +0200

    Fix

--- merge_preds_and_vals.py
+++ merge_preds_and_vals.py
@@ -26,3 +26,16 @@ merge_format_long("results_processed_merged_seq_no_val", 3, 24)
 merge_format_long("results_processed_20_seq_no_val", 5, 5)
 merge_format("results_processed_seq_no_test", 5, 5)
 merge_format("results_processed_seq_long", 5, 10)
+
+merge_format("results_processed_seq_genetic_low", 6, 10)
+merge_format("results_processed_seq_genetic_low_0", 6, 6)
+merge_format("results_processed_seq_genetic_low_1", 10, 10)
+
+merge_format("results_processed_seq_genetic_strong", 6, 10)
+merge_format("results_processed_seq_genetic_strong_0", 6, 10)
+merge_format("results_processed_seq_genetic_strong_1", 8, 10)
+
+merge_format("results_processed_seq_genetic_experiments_all", 9, 10)
+merge_format("results_processed_seq_genetic_experimentsA", 9, 10)
+merge_format("results_processed_seq_genetic_experimentsB", 9, 10)
+merge_format("results_processed_seq_genetic_experimentsC", 9, 10)
--- read_txt.py
+++ read_txt.py
@@ -7,11 +7,13 @@ def process_txt(txt_name, all_peps, all_labs, label_use = 1):
     pep_per_line = dict()
     for line in lines_txt:
         peps_in_line = []
-        if "\\texttt{" in line:
-            new_line = line.split("\\texttt{")
-            for pep in new_line:
-                if "}" in pep:
-                    peps_in_line.append(pep.split("}")[0])
+        if "&" in line:
+            cells = line.split("&")
+            for cell_ix in range(len(cells)):
+                if "\\texttt{" in cells[cell_ix]:
+                    peps_in_line.append(cells[cell_ix].split("\\texttt{")[-1].split("}")[0])
+                else:
+                    peps_in_line.append("")
         for ix in range(len(peps_in_line)):
             if ix not in pep_per_line:
                 pep_per_line[ix] = []
@@ -19,17 +21,25 @@ def process_txt(txt_name, all_peps, all_labs, label_use = 1):
     all_peps_me = []
     for keyval in pep_per_line:
         df_new = pd.DataFrame()
-        df_new['Feature'] = pep_per_line[keyval]
-        df_new['Label'] = [label_use for p in pep_per_line[keyval]]
-        df_new.to_csv(txt_name.replace(".txt", "_" + str(keyval) + ".csv"))
+        pf = []
+        pl = []
         for p in pep_per_line[keyval]:
-            all_peps.append(p)
-            all_labs.append(label_use)
-            all_peps_me.append(p)
+            if len(p) > 0:
+                pf.append(p)
+                pl.append(label_use)
+                all_peps.append(p)
+                all_labs.append(label_use)
+                all_peps_me.append(p)
+        if len(pf) > 0:
+            df_new['Feature'] = pf
+            df_new['Label'] = pl
+            df_new.to_csv(txt_name.replace(".txt", "_" + str(keyval // 2) + ".csv"))
+            print(txt_name, keyval, len(pf))
     df_new = pd.DataFrame()
     df_new['Feature'] = all_peps_me
     df_new['Label'] = [label_use for p in all_peps_me]
     df_new.to_csv(txt_name.replace(".txt", "_all.csv"))
+    print(txt_name, len(all_peps_me))
     return all_peps, all_labs
 
 all_peps, all_labs = process_txt("text_strong.txt", [], [])
@@ -39,3 +49,4 @@ df_new = pd.DataFrame()
 df_new['Feature'] = all_peps
 df_new['Label'] = all_labs
 df_new.to_csv("genetic_peptides.csv")
+print(len(all_peps))

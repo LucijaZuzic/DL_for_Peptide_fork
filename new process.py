00cf5ff219f2f4c4422140319f00eeaecd4465e6
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-10-01 06:49:47 +0200

    Create new process.py

---
+++ new process.py
@@ -0,0 +1,5 @@
+filenames_to_process = ["results_processed_20_seq_no_val/5_5/5_5_all_preds.csv",
+                        "results_processed_random_forest_20/5_5/5_5_all_preds.csv",
+                        "results_processed_merged_seq_no_val/3_24/3_24_all_preds.csv",
+                        "results_processed_random_forest/3_24/3_24_all_preds.csv",
+                        "results_processed_seq_long_no_val/5_10/5_10_all_preds.csv",]
